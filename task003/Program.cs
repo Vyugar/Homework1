43c07ca9f0cf003c3468d253c9b44a3fd3ac79b0
Vyugar <vyugar@yandex.ru>
2022-11-20 19:41:13 +0300

    modifed task003

--- task003/Program.cs
+++ task003/Program.cs
@@ -7,6 +7,10 @@ int userNumber = new int();
 Console.WriteLine("Введите число");
 userNumber = Convert.ToInt32(Console.ReadLine());
 if (userNumber % 2 == 0)
-    Console.WriteLine("Да (введено четное число)");
+    {
+        Console.WriteLine("Да (введено четное число)");
+    }
 else
+    {
     Console.WriteLine("Нет (введено нечетное число)");
+    }
